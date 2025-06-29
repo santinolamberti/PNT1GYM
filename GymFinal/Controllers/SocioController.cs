48d80e2be17173c08a64c2e283bd08e42f7c07a8
Santino Lamberti <santilamberti4@gmail.com>
2025-06-29 10:27:41 -0300

    cambios santi

---
+++ GymFinal/Controllers/SocioController.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using GymFinal.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace GymFinal.Controllers
+{
+    public class SocioController : Controller
+    {
+        private readonly GimnasioContext _context;
+
+        public SocioController()
+        {
+            _context = new GimnasioContext();
+        }
+
+        // GET: /Socio
+        public IActionResult Index(int? sedeId)
+        {
+            var socios = _context.Socios
+                .Include(s => s.Plan)
+                .Include(s => s.Sede)
+                .AsQueryable();
+
+            if (sedeId.HasValue)
+                socios = socios.Where(s => s.IdSede == sedeId.Value);
+
+            ViewBag.Sedes = _context.Sedes.ToList();
+            return View(socios.ToList());
+        }
+
+        // GET: /Socio/Create
+        public IActionResult Create()
+        {
+            ViewBag.Planes = _context.Planes.ToList();
+            ViewBag.Sedes = _context.Sedes.ToList();
+            return View();
+        }
+
+        // POST: /Socio/Create
+        [HttpPost]
+        public IActionResult Create(Socio socio)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Socios.Add(socio);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Planes = _context.Planes.ToList();
+            ViewBag.Sedes = _context.Sedes.ToList();
+            return View(socio);
+        }
+
+        // GET: /Socio/Edit/5
+        public IActionResult Edit(int id)
+        {
+            var socio = _context.Socios.Find(id);
+            if (socio == null) return NotFound();
+
+            ViewBag.Planes = _context.Planes.ToList();
+            ViewBag.Sedes = _context.Sedes.ToList();
+            return View(socio);
+        }
+
+        // POST: /Socio/Edit
+        [HttpPost]
+        public IActionResult Edit(Socio socio)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Socios.Update(socio);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Planes = _context.Planes.ToList();
+            ViewBag.Sedes = _context.Sedes.ToList();
+            return View(socio);
+        }
+
+        // GET: /Socio/Delete/5
+        public IActionResult Delete(int id)
+        {
+            var socio = _context.Socios.Find(id);
+            if (socio != null)
+            {
+                _context.Socios.Remove(socio);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ GymFinal/Models/GimnasioContext.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+namespace GymFinal.Models
+{
+        public class GimnasioContext : DbContext
+        {
+            public DbSet<Socio> Socios { get; set; }
+            public DbSet<Plan> Planes { get; set; }
+            public DbSet<Sede> Sedes { get; set; }
+            public DbSet<Empleado> Empleados { get; set; }
+            public DbSet<Actividad> Actividades { get; set; }
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseSqlServer("Data Source=DESKTOP-MRM7BSP;Initial Catalog=GimnasioDB;" +
+                    " Integrated Security=true; TrustServerCertificate=true; Encrypt=true");
+
+                base.OnConfiguring(optionsBuilder);
+            }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Relación muchos-a-muchos sin cascada
+            modelBuilder.Entity<Empleado>()
+                .HasMany(e => e.Actividades)
+                .WithMany(a => a.Empleados)
+                .UsingEntity(j => j.ToTable("EmpleadoActividad"));
+
+            // Desactivar cascada para evitar conflicto
+            modelBuilder.Entity<Actividad>()
+                .HasOne(a => a.Sede)
+                .WithMany(s => s.Actividades)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Empleado>()
+                .HasOne(e => e.Sede)
+                .WithMany(s => s.Empleados)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
